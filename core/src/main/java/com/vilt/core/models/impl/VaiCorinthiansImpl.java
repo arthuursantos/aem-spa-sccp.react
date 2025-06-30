37b57823630d6a585c968fde7325955faf657b6a
Arthur Santos <arthur.santos@vilt.local>
2025-06-29 23:18:31 -0300

    chore: first commit

---
+++ core/src/main/java/com/vilt/core/models/impl/VaiCorinthiansImpl.java
@@ -0,0 +1,33 @@
+package com.vilt.core.models.impl;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.vilt.core.models.VaiCorinthians;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(
+        adaptables= SlingHttpServletRequest.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = VaiCorinthiansImpl.RESOURCE_TYPE,
+        adapters = ComponentExporter.class
+)
+@Exporter(
+        name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+        extensions = ExporterConstants.SLING_MODEL_EXTENSION
+)
+public class VaiCorinthiansImpl implements VaiCorinthians {
+
+    protected static final String RESOURCE_TYPE = "aem-spa-sccp/components/vaicorinthians";
+
+    @ValueMapValue
+    private String title;
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ ui.frontend/src/components/vaicorinthians/vaicorinthians.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class vaicorinthians extends Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.text}</p>
+            </div>
+        )
+    }
+}
+
+export default vaicorinthians;
